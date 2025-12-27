5c886268bb519dcbecd2c7b63319f3cf0c98a8ca
= <=>
2025-12-27 00:56:41 -0300

    Criando funções para aplicar desconto e juros, função de verificação de forma de pagamento e função main para chamada das demais funções

--- funções/ex02.js
+++ funções/ex02.js
@@ -1,19 +1,27 @@
 let precoEtiqueta = 100;
 let formaDePagamento = Number(prompt("Digite a forma de pagamento (1 - à vista dinheiro/cheque, 2 - à vista cartão, 3 - 2x no cartão, 4 - 3x ou mais no cartão):"));
+function aplicarDesconto(preco, desconto) {
+    return preco - (preco * desconto);
+}
+
+function aplicarJuros(preco, juros) {
+    return preco + (preco * juros);
+}
+
 function calcularPrecoFinal(preco, formaPagamento) {
     let precoFinal;
     switch (formaPagamento) {
         case 1:
-            precoFinal = preco * 0.9; // 10% de desconto
+            precoFinal = aplicarDesconto(preco, 0.1); // 10% de desconto
             break;
         case 2:
-            precoFinal = preco * 0.85; // 15% de desconto
+            precoFinal = aplicarDesconto(preco, 0.15); // 15% de desconto
             break;
         case 3:
             precoFinal = preco; // preço normal
             break;
         case 4:
-            precoFinal = preco * 1.1; // 10% de juros
+            precoFinal = aplicarJuros(preco, 0.1); // 10% de juros
             break;
         default:
             alert("Forma de pagamento inválida.");
@@ -22,8 +30,9 @@ function calcularPrecoFinal(preco, formaPagamento) {
     return precoFinal;
 }
 
-let precoFinal = calcularPrecoFinal(precoEtiqueta, formaDePagamento);
-if (precoFinal !== null) {
-    alert("O preço final é R$ " + precoFinal.toFixed(2));
-}
-
+(function main() {
+    let precoFinal = calcularPrecoFinal(precoEtiqueta, formaDePagamento);
+    if (precoFinal !== null) {
+        alert("O preço final é R$ " + precoFinal.toFixed(2));
+    }
+})();
