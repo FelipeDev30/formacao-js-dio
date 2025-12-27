ffbe91dba400cc8a673fd7c61ed849aeee122e00
= <=>
2025-12-27 02:14:51 -0300

    Corrigindo comentario

--- Objetos/aula02.js
+++ Objetos/aula02.js
@@ -14,4 +14,4 @@ const pessoa1 = new Pessoa("Ana", 25, "desenvolvedora");
 pessoa1.apresentar(); // Olá, meu nome é Ana, tenho 25 anos e sou desenvolvedora.
 
 const pessoa2 = new Pessoa("Bruno", 30, "designer");
-pessoa2.apresentar(); // Olá, meu nome é Bruno, tenho 30 anos e sou designer.git a
+pessoa2.apresentar(); // Olá, meu nome é Bruno, tenho 30 anos e sou designer.
