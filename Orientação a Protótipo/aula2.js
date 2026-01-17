87e1bda96ac02aaa5e82352cf9efa90c909031f0
= <=>
2026-01-17 02:03:48 -0300

    Testando formas de passar parâmetros e argumentos para uma classe

--- Orientação a Protótipo/aula2.js
+++ Orientação a Protótipo/aula2.js
@@ -1,11 +1,28 @@
-function Pessoa(nome, idade) {
-    this.nome =  prompt('Informe seu nome: ')
-    this.idade = Number(prompt('Informe sua idade: '))
+class Pessoa {
+    constructor(nome, idade) {
+        nome = prompt('Informe seu nome: ')
+        idade = Number(prompt('Informe sua idade: '))
+        this.nome = nome
+        this.idade = idade
+    }
+
+    falar() {
+        alert(`Seu nome é: ${pessoa1.nome} e você tem ${pessoa1.idade} anos de idade!`)
+    }
 }
 
-Pessoa.prototype.falar = function() {
+
+/* 
+
+Pessoa.prototype.falar = function () {
+    function Pessoa() {
+        this.nome = prompt('Informe seu nome: ');
+        this.idade = Number(prompt('Informe sua idade: '));
+    }
     alert(`Seu nome é: ${pessoa1.nome} e você tem ${pessoa1.idade} anos de idade!`)
 }
+
+*/
 
 const pessoa1 = new Pessoa()
 
