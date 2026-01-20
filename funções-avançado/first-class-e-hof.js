9dd0523b6a992d433be11eaadcd979a991583509
= <=>
2026-01-20 14:36:33 -0300

    Entendendo a diferença entra functions e arrow functions, Arrow functions não cria um contexto próprio this, herda do contexto léxico, ou seja, do escopo onde foi definida

--- funções-avançado/first-class-e-hof.js
+++ funções-avançado/first-class-e-hof.js
@@ -1,16 +1,22 @@
-const hello = () => {
-    console.log('Hello World!');
-}
-
-function funcao() {
+function funcao1() {
     console.log(this);
 }
 
-const renan = {
-    nome: "Renan",
-    funcao,
-    hello,
+const funcao2 = () => {
+    console.log(this); // Não cria seu próprio this, herda do contexto léxico, ou seja, do escopo onde foi definida
 }
 
-renan.funcao()
-renan.hello()
+const felipe = {
+    nome: 'Felipe',
+    funcao1: funcao1,
+    funcao2: funcao2,
+    funcao3: function() {
+        console.log(this.nome);
+    }
+}
+
+felipe.funcao1(); // this aponta para o objeto felipe
+felipe.funcao2(); // this aponta para o contexto léxico (neste caso, o objeto global)
+felipe.funcao3(); // this aponta para o objeto felipe
+
+funcao2();
